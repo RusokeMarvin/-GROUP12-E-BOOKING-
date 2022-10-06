12050b0a155011278f450fca0229d52a2f8a7b8e
davidmuhumuza <107501786+davidmuhumuza@users.noreply.github.com>
2022-10-06 13:45:43 +0300

    sms and email test
    testing communication

---
+++ email and sms.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter/cupertino.dart'; Unused Dependency
+import 'package:url_launcher/url_launcher.dart';
+
+void main() => runApp(const SignInScreen(
+      title: 'sms',
+    ));
+
+_sendingMails() async {
+  var url = Uri.parse("mailto:dmuhumuza88@gmail.com");
+  if (await canLaunchUrl(url)) {
+    await launchUrl(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+_sendingSMS() async {
+  var url = Uri.parse("sms:0773078427");
+  if (await canLaunchUrl(url)) {
+    await launchUrl(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({Key? key, required String title}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+    home: Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                height: 200.0,
+              ),
+              const Text(
+                'Welcome to Gmusic app!',
+                style: TextStyle(
+                  fontSize: 35.0,
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                height: 20.0,
+              ),
+              const Text(
+                'For any Queries, Mail us',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.green,
+                  //fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                height: 10.0,
+              ),
+              ElevatedButton(
+                onPressed: _sendingMails,
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all(const EdgeInsets.all(5.0)),
+                  textStyle: MaterialStateProperty.all(
+                    const TextStyle(color: Colors.black),
+                  ),
+                ),
+                child: const Text('Here'),
+              ), // ElevatedButton
+
+              Container(
+                height: 20.0,
+              ),
+              const Text(
+                'Or Send SMS',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.green,
+                  //fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                height: 10.0,
+              ),
+              ElevatedButton(
+                onPressed: _sendingSMS,
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all(const EdgeInsets.all(5.0)),
+                  textStyle: MaterialStateProperty.all(
+                    const TextStyle(color: Colors.black),
+                  ), //ElevatedButtton
+                ),
+                child: const Text('Here'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
