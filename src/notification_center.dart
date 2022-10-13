bbf16ebfb7687bc3a0809b0b12514970a2d15bd0
ssoziivan <113168898+ssoziivan@users.noreply.github.com>
2022-10-13 19:21:27 +0300

    Add files via upload
    this is notification code

---
+++ src/notification_center.dart
@@ -0,0 +1,113 @@
+import 'package:meta/meta.dart';
+
+//  Created by WHC on 18/12/25.
+//  Copyright © 2018年 WHC. All rights reserved.
+//
+//  Github <https://github.com/netyouli/Flutter-NotificationCenter>
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+class NotificationCenter {
+  static NotificationCenter _default = NotificationCenter();
+  Map _observerMap = Map();
+  final _segmentKey = '-888-';
+
+  /**
+     * 发送通知
+     * name 通知名称
+     * param 通知参数
+     */
+  static void post({
+    required String name,
+    Object? param,
+  }) {
+    NotificationCenter._default._observerMap.forEach((key, value) {
+      final array = key.split(NotificationCenter._default._segmentKey);
+      if (array.length == 2) {
+        final _name = array[0];
+        if (_name == name && value != null) {
+          value(param);
+        }
+      }
+    });
+  }
+
+  /**
+     * 监听通知
+     * observer 监听对象
+     * name 监听的通知名称
+     * block 监听通知的回调
+     */
+  static void addObserver({
+    required Object observer,
+    required String name,
+    void Function(Object param)? block,
+  }) {
+    final key = name +
+        NotificationCenter._default._segmentKey +
+        observer.hashCode.toString();
+    NotificationCenter._default._observerMap[key] = block;
+  }
+
+  /**
+     * 移除通知
+     * observer 要移除的对象
+     * name 要移除的通知名称（如果为空则移除observer下所有通知）
+     */
+  static void removeObserver({
+    required Object observer,
+    String? name,
+  }) {
+    if (name != null) {
+      final key = name +
+          NotificationCenter._default._segmentKey +
+          observer.hashCode.toString();
+      NotificationCenter._default._observerMap.remove(key);
+    } else {
+      final keys = NotificationCenter._default._observerMap.keys;
+      final List<String> keysToRemove = [];
+
+      for (var key in keys) {
+        final array = key.split(NotificationCenter._default._segmentKey);
+        if (array.length == 2) {
+          final hasCode = array[1];
+          if (hasCode == observer.hashCode.toString()) {
+            keysToRemove.add(key);
+          }
+        }
+      }
+
+      NotificationCenter._default._observerMap
+          .removeWhere((key, value) => keysToRemove.contains(key));
+    }
+  }
+
+  @visibleForTesting
+  static bool hasObserver({required Object observer}) {
+    final keys = NotificationCenter._default._observerMap.keys;
+
+    return keys.any((key) {
+      final array = key.split(NotificationCenter._default._segmentKey);
+      if (array.length == 2) {
+        return observer.hashCode.toString() == array[1];
+      } else {
+        return false;
+      }
+    });
+  }
+}
