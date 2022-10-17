1adb8792c0dee425a81d8344e149991a127e0a6f
OLIVeNAKs <106664357+OLIVeNAKs@users.noreply.github.com>
2022-10-17 16:05:21 +0300

    updates on payment API

--- lib/pay _integrate.dart
+++ lib/pay _integrate.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: avoid_web_libraries_in_flutter
 
 import 'dart:js' as js;
-import 'package:flutter_web_paystack_popup/constant/payment_key.dart';
+//import 'package:flutter_web_paystack_popup/constant/payment_key.dart';
 
 import '../services/paystack_interop.dart' as paystack;
 
